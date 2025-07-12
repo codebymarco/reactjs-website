c8e4b6a7cea243eaf5c40ae79a65322c9bc0afe7
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2025-07-12 21:30:54 +0200

    Update User.jsx

--- src/components/navbar/User.jsx
+++ src/components/navbar/User.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const User = () => {
   return (
-    <div>User</div>
+    <div>
+      <h1 className="logo">logo</h1>
+      <div className="links">
+        <Link to='/'>home</Link>
+      </div>
+    </div>
   )
 }
 
